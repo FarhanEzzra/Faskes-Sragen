6d4eccbc4f397cb891fff531caa027bff8b186a7
FarhanEzzra <75241410+FarhanEzzra@users.noreply.github.com>
2020-12-01 16:03:27 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,19 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 111.023087820295018, -7.423151186914786 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 111.09844600443823, -7.485744503131602 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.889434780018391, -7.437042766286869 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.856689688192589, -7.378317177405003 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 111.09531083607196, -7.265507691556599 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.833698453506415, -7.347051254053826 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.927405152454668, -7.32010206284917 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 111.121437239124404, -7.381944572621198 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 111.087995443217224, -7.341177851456013 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.966768933053757, -7.488680249962662 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.972342565704949, -7.326321251719548 ] } },
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.943777698367555, -7.409580891562077 ] } }
+]
+}
